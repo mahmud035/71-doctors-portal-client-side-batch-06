a1e9cbed986ac0d6f01fdb2feb9f78fa47ece232
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-11 23:24:33 +0600

    71-4 Create simple Top Banner Using Hero component

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import chair from '../../../assets/images/chair.png';
 
 const Banner = () => {
   return (
-    <div>
-      <h2>Banner</h2>
+    <div className="  hero h-[500px]">
+      <div className=" hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} className="w-1/2 rounded-lg shadow-2xl" alt="" />
+
+        <div>
+          <h1 className="text-5xl font-bold">
+            Your New Smile Starts <br />
+            Here
+          </h1>
+
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#3A4256',
+
+          'base-100': '#F6F7F9',
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
