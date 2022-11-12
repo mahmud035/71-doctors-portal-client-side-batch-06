cf22a865eb95979d197f72aba6904df2cf2e5fdb
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-12 10:55:41 +0600

    added banner section background image

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png';
 import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
+import bannerBg from '../../../assets/images/bg.png';
 
 const Banner = () => {
   return (
-    <div className="hero">
+    <div
+      className="hero"
+      style={{
+        background: `url(${bannerBg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'left',
+      }}
+    >
       <div className=" hero-content flex-col lg:flex-row-reverse">
         <img src={chair} className=" lg:w-1/2 rounded-lg shadow-2xl" alt="" />
 
