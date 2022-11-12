0ad40c5677dd1c347cbe348ac6a9913d2d8533ad
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-12 10:51:17 +0600

    71-9 Module Summary and set Footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import footer from '../../../assets/images/footer.png';
 
 const Footer = () => {
   return (
-    <div>
-      <h5>Footer</h5>
-    </div>
+    <footer
+      className=" p-10 "
+      style={{
+        background: `url(${footer})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="footer">
+        <div>
+          <span className="footer-title">Services</span>
+          <Link to="/">Branding</Link>
+          <Link to="/">Design</Link>
+          <Link to="/">Marketing</Link>
+          <Link to="/">Advertisement</Link>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <Link to="/">About us</Link>
+          <Link to="/">Contact</Link>
+          <Link to="/">Jobs</Link>
+          <Link to="/">Press kit</Link>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <Link to="/">Terms of use</Link>
+          <Link to="/">Privacy policy</Link>
+          <Link to="/">Cookie policy</Link>
+        </div>
+      </div>
+      <div className="pt-8 text-sm text-center ">
+        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+      </div>
+    </footer>
   );
 };
 
