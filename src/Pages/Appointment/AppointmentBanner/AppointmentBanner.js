d86347471fff1f71ac283427ef0ca90ca6491eac
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-12 21:04:25 +0600

    72-2 Create Appointment Banner using React Day Picker

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../../assets/images/chair.png';
 import bannerBg from '../../../assets/images/bg.png';
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
 
 const AppointmentBanner = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <header className="py-40">
       <div
@@ -15,7 +19,14 @@ const AppointmentBanner = () => {
       >
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={chair} className="lg:w-1/2  rounded-lg shadow-2xl" alt="" />
-          <div></div>
+          <div className="mr-10">
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            />
+            <p>You have selected date: {format(selectedDate, 'PP')}</p>
+          </div>
         </div>
       </div>
     </header>
