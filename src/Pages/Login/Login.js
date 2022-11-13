1a4c9ebd8e43a88ae428d68d9c28894c456eeb2b
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-13 22:42:14 +0600

    73-3 Explore React hook form validation, error handling

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,13 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  console.log(errors);
 
   const handleLogin = (data) => {
     console.log(data);
@@ -19,10 +25,16 @@ const Login = () => {
               <span className="label-text">Email</span>
             </label>
             <input
-              {...register('email')}
-              type="text"
+              {...register('email', { required: 'Email Address is required' })}
+              type="email"
               className="input input-bordered w-full max-w-xs"
             />
+
+            {errors.email && (
+              <p role="alert" className="text-red-600 text-sm">
+                {errors.email?.message}
+              </p>
+            )}
           </div>
 
           <div className="form-control w-full max-w-xs">
@@ -30,10 +42,23 @@ const Login = () => {
               <span className="label-text">Password</span>
             </label>
             <input
-              {...register('password')}
+              {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 6,
+                  message: 'Password must be at least 6 characters',
+                },
+              })}
               type="password"
               className="input input-bordered w-full max-w-xs"
             />
+
+            {errors.password && (
+              <p role="alert" className="text-red-600 text-sm">
+                {errors.password?.message}
+              </p>
+            )}
+
             <label className="label">
               <span className="label-text">Forget Password?</span>
             </label>
