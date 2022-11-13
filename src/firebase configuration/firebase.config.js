82a6e2bc7391e70d55603ba57faa12edc620d885
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-13 18:54:54 +0600

    added firebase configuration

---
+++ src/firebase configuration/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCsYsz2ezrHSoXYwLK2VrlrTI6J2z13MGA',
+  authDomain: 'doctors-portal-client-si-27126.firebaseapp.com',
+  projectId: 'doctors-portal-client-si-27126',
+  storageBucket: 'doctors-portal-client-si-27126.appspot.com',
+  messagingSenderId: '219429203557',
+  appId: '1:219429203557:web:b4bf4283335a97a5f1608c',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
