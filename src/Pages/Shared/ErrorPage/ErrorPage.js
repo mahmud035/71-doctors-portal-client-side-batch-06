12c348e4ff300b6547af768b7c268303e1d42915
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-22 10:48:31 +0600

    show error message on Error Page

--- src/Pages/Shared/ErrorPage/ErrorPage.js
+++ src/Pages/Shared/ErrorPage/ErrorPage.js
@@ -8,8 +8,12 @@ const ErrorPage = () => {
   return (
     <div>
       {error && (
-        <div className="flex justify-center items-center flex-col gap-8 h-screen">
+        <div className="flex justify-center items-center flex-col gap-6 h-screen">
           <img src={Error404} alt="" className="w-[350px]" />
+          <p className="text-red-600 font-bold">
+            <i>{error.statusText || error.message}</i>
+          </p>
+
           <Link to="/">
             <button className="btn btn-info">Back To Home Page</button>
           </Link>
