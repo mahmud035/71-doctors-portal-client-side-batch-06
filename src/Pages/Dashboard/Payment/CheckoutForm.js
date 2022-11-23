d43ca3e71a38a3782d911888792480dd2c65cfe6
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-23 10:19:37 +0600

    77-8 Display payment error and success Message

--- src/Pages/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -3,7 +3,11 @@ import React, { useEffect, useState } from 'react';
 
 const CheckoutForm = ({ booking }) => {
   const [cardError, setCardError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState('');
   const [clientSecret, setClientSecret] = useState('');
+
   const stripe = useStripe();
   const elements = useElements();
   const { price, patientName, email } = booking;
@@ -48,6 +52,9 @@ const CheckoutForm = ({ booking }) => {
       setCardError('');
     }
 
+    setSuccess(''); // reset state defaultValue
+    setProcessing(true); // change state defaultValue
+
     const { paymentIntent, error: confirmError } =
       await stripe.confirmCardPayment(clientSecret, {
         payment_method: {
@@ -58,6 +65,22 @@ const CheckoutForm = ({ booking }) => {
           },
         },
       });
+
+    if (confirmError) {
+      setCardError(confirmError.message);
+      return;
+    }
+
+    console.log('paymentIntent', paymentIntent);
+
+    if (paymentIntent.status === 'succeeded') {
+      setSuccess('Congrats! your payment is completed');
+      setTransactionId(paymentIntent.id);
+      // store payment info in the database
+    }
+
+    //  Changing the state of the `processing` variable to `false`
+    setProcessing(false);
   };
 
   return (
@@ -82,13 +105,22 @@ const CheckoutForm = ({ booking }) => {
         <button
           className="btn btn-sm mt-4 btn-primary"
           type="submit"
-          disabled={!stripe || !clientSecret}
+          disabled={!stripe || !clientSecret || processing}
         >
           Pay
         </button>
       </form>
 
       <p className="text-red-500">{cardError}</p>
+      {success && (
+        <div>
+          <p className="text-green-500">{success}</p>
+          <p>
+            Your transactionId:
+            <span className="font-bold">{transactionId}</span>
+          </p>
+        </div>
+      )}
     </>
   );
 };
